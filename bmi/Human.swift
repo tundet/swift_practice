a377771f6785a58400df2d6ba537645065125740
tundet <ryunde.taba@gmail.com>
2017-03-23 23:38:07 +0200

    Create Human.swift

---
+++ bmi/Human.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class Human: ViewController{
+    var height:Int = 0
+    var weight:Int = 0
+    var bmi:Double = 0.0
+    var delegate: ProtocolTest?
+    
+    
+    func calculateBMI(height:Int, weight:Int){
+        self.height = height
+        self.weight = weight
+        let bmiHeight = Double(height)/100
+        self.bmi = Double(weight)/(bmiHeight*bmiHeight)
+        
+        delegate?.updateBMI(controller: self, bmi: self.bmi)
+        
+    }
+    
+}
